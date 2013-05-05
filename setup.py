5a8160923fe157a5e334286d73250ff2349e98f0
Balazs Ree <ree@greenfinity.hu>
2013-05-05 16:01:41 +0200

    It looks like package_data is also needed in setup.py. Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='firebase-token-generator',
-    version='1.3',
+    version='1.4',
     author='Greg Soltis',
     author_email='greg@firebase.com',
     zip_safe=False,
@@ -10,5 +10,8 @@ setup(
     license='LICENSE',
     url='https://github.com/firebase/firebase-token-generator-python',
     description='A utility to generate signed Firebase Authentication Tokens',
-    long_description=open('README.md').read()
+    long_description=open('README.md').read(),
+    package_data={
+        '': ['README.md'],
+    },
 )
